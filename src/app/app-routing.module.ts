538c1da49c52158257a944b28c25423d0075dc14
Daniel Velázquez <dvelazquez1282@gmail.com>
2022-10-19 22:07:22 +0200

    - Added route for login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
