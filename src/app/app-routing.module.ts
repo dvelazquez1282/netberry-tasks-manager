cb33e94640b1356be5bb690ca6227d96babdb807
Daniel Velázquez <dvelazquez1282@gmail.com>
2022-10-22 16:20:23 +0200

    - Use of the guard in the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'panel', 
+    canActivate: [AuthGuard],
+    children: [
+
+    ]
+  },
 ];
 
 @NgModule({
