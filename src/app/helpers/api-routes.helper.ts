5789f91fa2b72060763f0ff054acea1271dff5c5
Daniel Velázquez <dvelazquez1282@gmail.com>
2022-10-22 18:05:03 +0200

    - Added routes for tasks api requests

--- src/app/helpers/api-routes.helper.ts
+++ src/app/helpers/api-routes.helper.ts
@@ -12,6 +12,8 @@ export class ApiRoutes {
     private static AUTH_LOGIN_ROUTE = '/login'
     private static AUTH_LOGOUT_ROUTE = '/logout'
 
+    private static TASK_PREFIX = '/tasks'
+
 
     public static getUserMeRoute() {
         return ApiRoutes.getAPIHostRoute() + ApiRoutes.USERS_PREFIX + ApiRoutes.USER_ME_ROUTE;
@@ -23,6 +25,22 @@ export class ApiRoutes {
 
     public static getAuthLogoutRoute() {
         return ApiRoutes.getAPIHostRoute() + ApiRoutes.AUTH_PREFIX + ApiRoutes.AUTH_LOGOUT_ROUTE;
+    }
+    
+    public static getListTaskRoute() {
+        return ApiRoutes.getAPIHostRoute() + ApiRoutes.TASK_PREFIX;
+    }
+    
+    public static getStoreTaskRoute() {
+        return ApiRoutes.getAPIHostRoute() + ApiRoutes.TASK_PREFIX;
+    }
+
+    public static getUpdateTaskRoute(taskId: number) {
+        return ApiRoutes.getAPIHostRoute() + ApiRoutes.TASK_PREFIX + '/' + taskId.toString();
+    }
+
+    public static getDeleteTaskRoute(taskId: number) {
+        return ApiRoutes.getAPIHostRoute() + ApiRoutes.TASK_PREFIX + '/' + taskId.toString();
     }
 
     private static getAPIHostRoute(): string {
