ae3c7c57dc2f3497255059b27b18390c6e853634
Daniel Velázquez <dvelazquez1282@gmail.com>
2022-10-22 15:26:06 +0200

    - Created login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './forms/login-form/login-form.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TextInputComponent } from './inputs/text-input/text-input.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Pages
+    LoginComponent,
+    // Forms
+    LoginFormComponent,
+    // Inputs
+    TextInputComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,    
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    {
+      provide : HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi   : true,
+    },    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
