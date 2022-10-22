1b108d5081ddd3db3e248d1b22e09d41520b6e57
Daniel Velázquez <dvelazquez1282@gmail.com>
2022-10-22 19:34:13 +0200

    - Update routing with tasks routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './pages/login/login.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
 const routes: Routes = [
+  { path: '',   redirectTo: '/panel/tasks', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
   { path: 'panel', 
     canActivate: [AuthGuard],
     children: [
-
+      { path: '',   redirectTo: '/panel/tasks', pathMatch: 'full'},
+      { path: 'tasks', component: TasksComponent,  },
     ]
   },
 ];
